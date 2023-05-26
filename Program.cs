3a35703fb66562ed1f24ecb9bd90368f058b0c22
thaynasantosss <thayna.2015.09@gmail.com>
2023-05-26 19:30:31 -0300

    TAREFA REALIZADA

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("Tabuada do número: ");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+int n = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+Console.WriteLine();
+
+int m = 0;
+int produto;
+
+while (m <= 10)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    produto = n * m;
+    Console.WriteLine($"{n} x {m} = {produto}");
+    m += 1;
+    Console.ResetColor();
+}
